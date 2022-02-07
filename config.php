553ccaddfcc8f7d890d8972bd169cf7dac810b07
the-web-wolf <abeshiemmanuel37@gmail.com>
2022-02-07 22:03:04 +0200

    test

--- config.php
+++ config.php
@@ -3,12 +3,15 @@ require('vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+
 // extract config variables from environment variables
 $db_host = $_ENV['DB_HOST'];
 $db_name = $_ENV['DB_NAME'];
 $db_user = $_ENV['DB_USER'];
 $db_pass = $_ENV['DB_PASS'];
 $db_port = $_ENV['DB_PORT'];
+
+die($db_host);
 
 // connect to postgresql and handle errors
 $conn = pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_pass") or die('Could not connect: ' . pg_last_error());
