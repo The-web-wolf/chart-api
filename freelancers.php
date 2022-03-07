0d6f1f8fe29adaa9ac817918c9994b0a032b9355
Emmanuel Abeshi <30579182+The-web-wolf@users.noreply.github.com>
2022-03-07 22:08:25 +0200

    Update freelancers.php

--- freelancers.php
+++ freelancers.php
@@ -8,7 +8,7 @@ header('Access-Control-Allow-Credentials: true');
 $db_name = "chart";
 $conn = pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_pass") or die('Could not connect: ' . pg_last_error());
 
-$query = "SELECT DISCTINCT worker_initials FROM 'new-chart'";
+$query = "SELECT DISCTINCT worker_initials FROM \"new-chart\" ";
 $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
 
 // print data
