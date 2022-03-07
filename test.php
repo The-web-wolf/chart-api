96394bdb9e605aa3ccc28f97df3c3f7129d6c37b
Emmanuel Abeshi <30579182+The-web-wolf@users.noreply.github.com>
2022-03-07 15:28:50 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Credentials: true');
 
 // read data from postgresql
-$query = "SELECT * FROM tasks WHERE is_active = 'f' AND worker_initials in (SELECT initials FROM workers WHERE is_bot = false) ORDER BY id ASC";
+$query = "SELECT * FROM tasks WHERE is_active = 'f' AND worker_initials in (SELECT worker_initials FROM freelancers WHERE is_bot = false and ready_for_receiving_tasks) ORDER BY id ASC";
 $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
 
 // print data
