29e75edb26114b96e4ff715ff108786c2ea23770
Emmanuel Abeshi <30579182+The-web-wolf@users.noreply.github.com>
2022-03-07 18:38:07 +0200

    Update test.php

--- test.php
+++ test.php
@@ -8,18 +8,22 @@ header('Access-Control-Allow-Credentials: true');
 $db_name = "chart";
 $conn = pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_pass") or die('Could not connect: ' . pg_last_error());
 
-$filter = $_GET['filter'];
-// sanitize input
-$filter = pg_escape_string($filter);
+if (isset($_GET['filter'])) {
+  $filter = $_GET['filter'];
+  // sanitize input
+  $filter = pg_escape_string($filter);
+} else {
+  $filter = "vat_report";
+}
 
 // read data from postgresql
-$query = 'SELECT * FROM "new-chart" WHERE name = '.$filter.' ';
+$query = 'SELECT * FROM "new-chart" WHERE name = ' . $filter . ' ';
 $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
 
 // print data
 $data = array();
 while ($row = pg_fetch_row($result)) {
-    $data[] = $row;
+  $data[] = $row;
 }
 
 print_r(json_encode($data));
