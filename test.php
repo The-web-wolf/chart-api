0093a46246c23666c07b9218b237565f0615549a
Emmanuel Abeshi <30579182+The-web-wolf@users.noreply.github.com>
2022-03-07 12:36:38 +0200

    Update test.php

--- test.php
+++ test.php
@@ -21,43 +21,5 @@ header('Access-Control-Allow-Credentials: true');
 $query = "SELECT * FROM report";
 $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
 
-// create object of results
-$receipts = array();
-
 $rows_count = pg_num_rows($result);
-for ($i = 0; $i < $rows_count; $i++) {
-    $row = pg_fetch_assoc($result);
-   // check if value of expense_type is already in receipts array
-    if(!in_array($row['expense_type'], $receipts)) {
-      $row_extract = array(
-        'expense' => $row['expense'],
-        'expense_date' => $row['expense_date'],
-        'amount' => $row['amount']
-      );
-      $receipts[$row['expense_type']][] = $row_extract;
-    }
-}
-
-
-// loop through receipts array and append to each expense type
-
-$merged_receipts = array();
-
-foreach ($receipts as $key => $value) {
-  $merged_receipts[$key] = array_reduce($value, function($carry, $item) {
-    // check if two expenses have same name
-      $carry[$item['expense']][] = // add all expenses with that name
-        array(
-          'expense_date' => $item['expense_date'],
-          'amount' => $item['amount']
-        );
-    return $carry;
-  });
-
-}
-
-
-print_r(json_encode($merged_receipts));
-
-// $receipts = json_encode($receipts);
-// print_r($receipts);
+print($rows_count)
