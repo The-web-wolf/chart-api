06e63f351c672164ad0f4108f05a54f89ada68ac
Emmanuel Abeshi <30579182+The-web-wolf@users.noreply.github.com>
2022-03-07 16:29:15 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,8 +5,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Credentials: true');
 
+
+$db_name = "chart";
+
 // read data from postgresql
-$query = "SELECT * FROM tasks WHERE is_active = 'f' AND worker_initials in (SELECT worker_initials FROM freelancers WHERE is_bot = false and ready_for_receiving_tasks) ORDER BY id ASC";
+$query = "SELECT * FROM `new-chart`";
 $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
 
 // print data
