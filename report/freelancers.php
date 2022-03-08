4c5225d93d7414bc77a901cbc567d981999a1a0b
the-web-wolf <abeshiemmanuel37@gmail.com>
2022-03-08 12:04:43 +0200

    Fixed route of include

--- report/freelancers.php
+++ report/freelancers.php
@@ -1,5 +1,5 @@
 <?php
-require('config.php');
+require('../config.php');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
